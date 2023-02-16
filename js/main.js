abcb1390e52be47b646de72efed08cc9fcbf8b5f
Pedro Henrique Mendes <peedrohenriqm@gmail.com>
2023-02-15 23:19:44 -0300

    feat

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+ const chaveAcesso = "a4b094f9fc73c79e3425effec9d1f9d8";
  const botao = document.querySelector("#btn");
+ const input = document.querySelector("#input");
 
  botao.addEventListener("click", (evento)=>{
-    console.log(evento)
+   let resultadoInput = input.value;
+
+   console.log(resultadoInput);
  })
